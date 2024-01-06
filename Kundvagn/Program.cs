2e2964f3ca1cfbd2148d8e36711fa320580b31c3
jbtand19 <jbtand19@gmail.com>
2024-01-06 18:18:10 +0100

    New start

--- Kundvagn/Program.cs
+++ Kundvagn/Program.cs
@@ -1,41 +1,141 @@
-﻿using Kundvagn;
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello");
-bool loop = false;
-var cartItems = new CartItem();
-var item1 = new Product { ProductId = 1, BrandId = 1, ProductName = "Melon" };
-var item2 = new Product { ProductId = 2, BrandId = 2, ProductName = "Banan" };
-do
+class Program
 {
-    Console.WriteLine("Buy\n1.Melon\n2.Apple");
-	var userInput = int.Parse(Console.ReadLine());
-	switch (userInput)
-	{
-        case 1:
-            Console.WriteLine("Melon");
-            cartItems.Products.Add(item1);
-            break;
-        case 2:
-            Console.WriteLine("Banan");
-            cartItems.Products.Add(item2);
-            break;
-        default:
-			break;
-	}
-    Console.WriteLine("Buy more");
-    var shopmore = int.Parse(Console.ReadLine());
-    if (shopmore == 1)
+    static void Main()
     {
-        loop = true;
-    }
-    else if(shopmore == 2)
-    {
-        loop = false;
-    }
-} while (loop);
+        // Skapa några varumärken
+        Brand apple = new Brand("Apple");
+        Brand orange = new Brand("Orange");
 
+        // Skapa några produkter och tilldela dem varumärken
+        Product appleProduct = new Product("Apple", apple, 2.5);
+        Product orangeProduct = new Product("Orange", orange, 1.8);
 
-foreach (var item in cartItems.Products)
+        // Skapa en lista med produkter
+        List<Product> products = new List<Product> { appleProduct, orangeProduct };
+
+        // Skapa en kundvagn
+        Cart cart = new Cart();
+
+        do
+        {
+            // Visa produktlistan för användaren
+            Console.WriteLine("Välj en produkt att lägga till i kundvagnen:");
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {products[i].Brand.Name} - {products[i].Name} - {products[i].Price} kr");
+            }
+
+            // Låt användaren välja en produkt
+            Console.Write("Ange produktens nummer: ");
+            int selectedProductIndex = int.Parse(Console.ReadLine()) - 1;
+
+            // Lägg till produkten i kundvagnen
+            cart.AddToCart(products[selectedProductIndex]);
+
+            // Visa kundvagnen
+            Console.WriteLine("Kundvagnen:");
+            foreach (CartItem cartItem in cart.CartItems)
+            {
+                Console.WriteLine($"{cartItem.Product.Brand.Name} - {cartItem.Product.Name} - {cartItem.Product.Price} kr  - {cartItem.Quantity}");
+            }
+
+            
+        } while (true);
+        // Ta bort en produkt från kundvagnen
+        Console.Write("Ange produktnummer att ta bort: ");
+        int productToRemoveIndex = int.Parse(Console.ReadLine()) - 1;
+        cart.RemoveFromCart(cart.CartItems[productToRemoveIndex].Product);
+
+        // Visa uppdaterad kundvagn
+        Console.WriteLine("Uppdaterad kundvagn:");
+        foreach (CartItem cartItem in cart.CartItems)
+        {
+            Console.WriteLine($"{cartItem.Product.Brand.Name} - {cartItem.Product.Name} - {cartItem.Product.Price} kr");
+        }
+
+    }
+}
+
+// Brand-klassen
+class Brand
 {
-    Console.WriteLine(item.ProductName);
+    public string Name { get; }
+
+    public Brand(string name)
+    {
+        Name = name;
+    }
+}
+
+// Product-klassen
+class Product
+{
+    public string Name { get; }
+    public Brand Brand { get; }
+    public double Price { get; }
+
+    public Product(string name, Brand brand, double price)
+    {
+        Name = name;
+        Brand = brand;
+        Price = price;
+    }
+}
+
+// CartItem-klassen
+class CartItem
+{
+    public Product Product { get; }
+    public int Quantity { get; set; }
+
+    public CartItem(Product product, int quantity)
+    {
+        Product = product;
+        Quantity = quantity;
+    }
+}
+
+// Cart-klassen
+class Cart
+{
+    public List<CartItem> CartItems { get; }
+
+    public Cart()
+    {
+        CartItems = new List<CartItem>();
+    }
+
+    public void AddToCart(Product product)
+    {
+        CartItem existingItem = CartItems.Find(item => item.Product == product);
+
+        if (existingItem != null)
+        {
+            existingItem.Quantity++;
+        }
+        else
+        {
+            CartItems.Add(new CartItem(product, 1));
+        }
+    }
+
+    public void RemoveFromCart(Product product)
+    {
+        CartItem existingItem = CartItems.Find(item => item.Product == product);
+
+        if (existingItem != null)
+        {
+            if (existingItem.Quantity > 1)
+            {
+                existingItem.Quantity--;
+            }
+            else
+            {
+                CartItems.Remove(existingItem);
+            }
+        }
+    }
 }
